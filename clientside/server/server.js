92d852fc3610ae13cbd230df336b450a7817f2e8
Zandrr <alexander.campbell@colorado.edu>
2015-01-26 12:44:46 -0700

    made it an express and angular app, I think...

--- clientside/server/server.js
+++ clientside/server/server.js
@@ -1,21 +1,29 @@
-var http     = require("http");
-var url      = require("url");
-var express  = require("express");
-var fs       = require("fs");
+var express         = require('express'),
+    http            = require('http'),
+    routes          = require('./routes'),
+    bodyParser      = require('body-parser'),
+    fs              = require('fs');
 
+fs.realpath(__dirname + '/../', function (err, projectRoot) {
   var app = express();
-  var port = 8888;
+  console.log(projectRoot);
 
-  function start(route, handle) {
-    function onRequest(request, response) {
-      var pathname = url.parse(request.url).pathname;
-      console.log("Request for " + pathname + " received.");
-      route(handle, pathname, response, request);
-    }
+  /* Set Up */
+  app.set('port', process.env.PORT || 5000);
+  app.set('views', projectRoot + '/server/views');
+  app.set('view engine', 'jade');
+  app.use(require('serve-static')(projectRoot + '/client/partials'));
+  app.use(require('serve-static')(projectRoot + '/client/src'));
+  app.use(require('serve-static')(projectRoot + '/client/assets'));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-    http.createServer(onRequest).listen(port);
-    console.log("Server starting on port "+port+"...");
-  }
+  var server = http.createServer(app);
+  routes(app); //seperate routes from main server
 
-  exports.start = start;
+  server.listen(app.get('port'), function(){
+    console.log("listening on port "+ app.get('port'));
+  });
 
+
+});
